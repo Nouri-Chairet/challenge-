1977a60a4dd54c7e501d458fb9d5e366e1366df0
Nour eddine chairet <142816182+Nouri-Chairet@users.noreply.github.com>
2024-11-22 22:32:48 +0100

    Update app.js

--- js recap/app.js
+++ js recap/app.js
@@ -4,8 +4,17 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
     const query = document.getElementById('search').value.trim();
     if (!query) return alert("Please enter a movie name!");
     const resultsDiv = document.getElementById('results');
+    // Challenge: Implement the searchMovies function to show the movies in the resultsDiv.
     // Hint 1: Use a try-catch block to handle potential errors.
-    // Hint 2: Fetch the movies using the fetchMovies function and sort them by year.
-    // Hint 3: Dynamically create and append movie elements to the resultsDiv.
-
+    // Hint 2: Fetch the movies using the fetchMovies function.
+    // Hint 3: Sort the movies by year.
+    // Hint 4: Dynamically create and append movie elements to the resultsDiv.
+    
 });
+/* Hint: The movie element should look like this: 
+<div class="movie">
+     <img src="" alt="" />
+     <h2></h2>
+     <p></p>
+     <p></p>
+</div>*/
